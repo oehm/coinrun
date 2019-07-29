46a1eade993fba0f38a3df172f92d8f17ec00245
oehm <thoffmann.mmt-m2015@fh-salzburg.ac.at>
2019-07-29 19:35:49 +0200

    seed based agents; real mutation; model saving fixed; first laerge scale test;

--- coinrun/evolve_agent.py
+++ coinrun/evolve_agent.py
@@ -3,8 +3,10 @@ Train an agent using a genetic algorithm.
 """
 
 import os
+import copy
 import time
 import numpy as np
+import random
 from mpi4py import MPI
 import tensorflow as tf
 from baselines.common import set_global_seeds
@@ -30,7 +32,7 @@ def main():
 
     nenvs = Config.NUM_ENVS
     #utils.mpi_print("nenvs", nenvs)
-    total_timesteps = int(1e5)
+    total_timesteps = int(2e3)
 
     datapoints = []
 
@@ -45,62 +47,85 @@ def main():
     policy = policies.get_policy()
 
     # setup graph
-    graph = tf.Graph()
-    with tf.Session(graph=graph) as sess:
-        model = policy(sess, env.observation_space, env.action_space, nenvs, 1)
-        model_init_op = tf.global_variables_initializer()
+    graph = tf.get_default_graph()
+    sess = tf.Session(graph=graph)
 
-        params = tf.trainable_variables()
-        params = [v for v in params if '/bias' not in v.name] # filter biases
+    model = policy(sess, env.observation_space, env.action_space, nenvs, 1)
+    model_init_op = tf.global_variables_initializer()
 
-        model_noise_ops = []
-        total_num_params = 0
-        for p in params:
-            shape = p.get_shape()
-            shape_list = shape.as_list()
-            num_params = np.prod(shape_list)
-            #utils.mpi_print('param', p, num_params)
-            total_num_params += num_params
+    params = tf.trainable_variables()
+    params = [v for v in params if '/bias' not in v.name] # filter biases
 
-            noise = tf.random_normal(shape, mean=0, stddev=1.0, dtype=tf.float32)
-            model_noise_ops.append(tf.assign_add(p, noise))
 
-        utils.mpi_print('total num params:', total_num_params)
+    model_noise_ops = []
+    total_num_params = 0
+    for p in params:
+        shape = p.get_shape()
+        shape_list = shape.as_list()
+        num_params = np.prod(shape_list)
+        #utils.mpi_print('param', p, num_params)
+        total_num_params += num_params
+
+        noise = tf.random_normal(shape, mean=0, stddev=0.01, dtype=tf.float32)
+        model_noise_ops.append(tf.assign_add(p, noise))
+
+    utils.mpi_print('total num params:', total_num_params)
     
+    def load_agent(agent):
+        set_global_seeds(agent["seed"])
+        sess.run(model_init_op)
+
+        for mut in agent["mut"]:
+            set_global_seeds(mut)
+            for model_noise_op in model_noise_ops:
+                sess.run(model_noise_op)
+
+    def run(timesteps):
+        obs = env.reset()
+        state = model.initial_state
+        done = np.zeros(nenvs)
+
+        rew_accum = 0
+        for i in range(timesteps):
+            action, values, state, _ = model.step( obs, state, done)
+            obs, rew, done, info = env.step(action)
+            rew_accum += rew
+        return sum(rew_accum)
+
+    load_data = Config.get_load_data('default')
+    if load_data is not None:
+        utils.load_all_params(sess)
+        for i in range(10):
+            utils.mpi_print(run(500))
+
     # initialise population
-    population_count = 6
+    population_count = 24
     timesteps_per_agent = 500
 
-    population = [{"sess":tf.Session(graph=graph), "fit": -1} for _ in range(population_count)]
+    population = [{"seed":random.randint(0,10000), "mut":[], "fit": -1} for _ in range(population_count)]
+
+    utils.mpi_print("== population size ", population_count, ", nenvs ", nenvs, ", t_agent ", timesteps_per_agent, " ==")
+
+
+    tfirststart = time.time()
 
     # main loop
     generation = 0
     timesteps_done = 0
     while timesteps_done < total_timesteps:
-        utils.mpi_print("__ generation ", generation, ", population size ", len(population), " __")
+        utils.mpi_print("__ gen ", generation, ", t_done ", timesteps_done, " __")
 
         # initialise and evaluate all new agents
         for agent in population:
             if agent["fit"] < 0:
-                agent["sess"].run(model_init_op)
-                utils.load_all_params(agent["sess"])
+                
+                load_agent(agent)
 
-                obs = env.reset()
-                state = model.initial_state
-                done = np.zeros(nenvs)
-
-                rew_accum = 0
-                for i in range(timesteps_per_agent):
-                    action, values, state, _ = model.step_with_sess(agent["sess"], obs, state, done)
-                    obs, rew, done, info = env.step(action)
-                    rew_accum += rew
-
-                #utils.mpi_print('rew_accum', rew_accum)
+                agent["fit"] = run(timesteps_per_agent)
 
                 timesteps_done += timesteps_per_agent
-                agent["fit"] = sum(rew_accum)
 
-        utils.mpi_print([agent["fit"] for agent in population])
+        utils.mpi_print([agent["fit"] for agent in population]) 
 
         # sort by fitness
         population.sort(key=lambda k: k['fit'], reverse=True) 
@@ -112,21 +137,29 @@ def main():
             break
 
         # replace weak agents
-        for agent in population[population_count//2:]:
-            agent["sess"].run(model_init_op)
-            #for model_noise_op in model_noise_ops:
-            #    agent["sess"].run(model_noise_op)
-            agent["fit"] = -1
+        for i in range(population_count//4):
+            source_agent = population[i]
+            for j in range(3):
+                target_agent = population[-i-1-(j * population_count//4)]
+                target_agent["seed"] = source_agent["seed"]
+                target_agent["mut"] = copy.deepcopy(source_agent["mut"])
+                target_agent["mut"].append(random.randint(0,10000))
+                target_agent["fit"] = -1
 
 
         generation += 1
     
+    utils.mpi_print(tfirststart - time.time())
+
     # save best performing agent
-    save_model(population[0]["sess"])
+    load_agent(population[0])
+    for i in range(10):
+        utils.mpi_print(run(500))
+
+    save_model(sess)
 
     # cleanup
-    for agent in population:
-        agent["sess"].close()
+    sess.close()
     return 0
 
 if __name__ == '__main__':
