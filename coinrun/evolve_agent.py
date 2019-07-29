021cb39c9ce7ef2c5c055c38f4d2c8338a4744ba
oehm <thoffmann.mmt-m2015@fh-salzburg.ac.at>
2019-07-29 14:40:03 +0200

    save model; output; session reusing;

--- coinrun/evolve_agent.py
+++ coinrun/evolve_agent.py
@@ -2,16 +2,19 @@
 Train an agent using a genetic algorithm.
 """
 
+import os
 import time
 import numpy as np
 from mpi4py import MPI
 import tensorflow as tf
 from baselines.common import set_global_seeds
 import coinrun.main_utils as utils
-from coinrun import setup_utils, policies, wrappers, ga
+from coinrun import setup_utils, policies, wrappers
 from coinrun.config import Config
 
 def main():
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
     args = setup_utils.setup_and_load()
 
     comm = MPI.COMM_WORLD
@@ -26,8 +29,14 @@ def main():
     config.gpu_options.allow_growth = True # pylint: disable=E1101
 
     nenvs = Config.NUM_ENVS
-    utils.mpi_print("nenvs", nenvs)
-    total_timesteps = int(1e4)
+    #utils.mpi_print("nenvs", nenvs)
+    total_timesteps = int(1e5)
+
+    datapoints = []
+
+    def save_model(sess, base_name=None):
+        base_dict = {'datapoints': datapoints}
+        utils.save_params_in_scopes(sess, ['model'], Config.get_save_file(base_name=base_name), base_dict)
 
     env = utils.make_general_env(nenvs, seed=rank)
 
@@ -48,10 +57,10 @@ def main():
     population = [{"sess":tf.Session(graph=graph), "fit": -1} for _ in range(population_count)]
 
     # main loop
+    generation = 0
     timesteps_done = 0
     while timesteps_done < total_timesteps:
-        utils.mpi_print("__________________")
-        utils.mpi_print("n pop", len(population))
+        utils.mpi_print("__ generation ", generation, ", population size ", len(population), " __")
 
         # initialise and evaluate all new agents
         for agent in population:
@@ -74,15 +83,28 @@ def main():
                 timesteps_done += timesteps_per_agent
                 agent["fit"] = sum(rew_accum)
 
+        utils.mpi_print([agent["fit"] for agent in population])
+
         # sort by fitness
         population.sort(key=lambda k: k['fit'], reverse=True) 
-        # kill weak agents
+
+        #
+        datapoints.append([generation, population[0]["fit"]])
+
+        if not timesteps_done < total_timesteps:
+            break
+
+        # replace weak agents
         for agent in population[population_count//2:]:
-            agent["sess"].close()
-        population = population[:population_count//2]
-        # create new agents
-        population += [{"sess":tf.Session(graph=graph), "fit": -1} for _ in range((population_count+1)//2)]
+            agent["sess"].run(model_initialiser)
+            agent["fit"] = -1
+
+
+        generation += 1
     
+    # save best performing agent
+    save_model(population[0]["sess"])
+
     # cleanup
     for agent in population:
         agent["sess"].close()
