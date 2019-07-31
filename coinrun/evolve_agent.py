01cf94de1d9a40c0a230ba5a3c76e65510223567
oehm <thoffmann.mmt-m2015@fh-salzburg.ac.at>
2019-07-31 22:14:21 +0200

    switched to tensorflow checkpoints; prettier code; prettier output;

--- coinrun/evolve_agent.py
+++ coinrun/evolve_agent.py
@@ -10,6 +10,7 @@ import math
 import numpy as np
 import random
 import uuid
+import re
 from mpi4py import MPI
 import joblib
 import tensorflow as tf
@@ -19,6 +20,7 @@ from coinrun import setup_utils, policies, wrappers
 from coinrun.config import Config
 
 def main():
+    # general setup
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
     args = setup_utils.setup_and_load()
@@ -42,20 +44,20 @@ def main():
 
     # params
     nenvs = Config.NUM_ENVS
-    #utils.mpi_print("nenvs", nenvs)
     total_timesteps = int(2e5)
     population_count = 32
     timesteps_per_agent = 500
 
+    # create environment
     env = utils.make_general_env(nenvs, seed=rank)
     env = wrappers.add_final_wrappers(env)
         
-    policy = policies.get_policy()
 
-    # setup graph
+    # setup model and all tensorflow ops
     graph = tf.get_default_graph()
     sess = tf.Session(graph=graph)
 
+    policy = policies.get_policy()
     model = policy(sess, env.observation_space, env.action_space, nenvs, 1)
     model_init_op = tf.global_variables_initializer()
 
@@ -73,36 +75,32 @@ def main():
 
         noise = tf.random_normal(shape, mean=0, stddev=0.01, dtype=tf.float32)
         model_noise_ops.append(tf.assign_add(p, noise))
-        # test normalisatiom
+        # TODO test normalisatiom
+
+    model_saver = tf.train.Saver(params)
 
     utils.mpi_print('total num params:', total_num_params)
 
     # function definitions
-    def save_model(name=None):
-        file_name = sub_dir + "/" + name if name else Config.get_save_file()
-        utils.save_params_in_scopes(sess, ['model'], file_name)
 
+    # dump compatible with coinruns file format
+    def dump_model():
+        utils.save_params_in_scopes(sess, ['model'], Config.get_save_file())
+
+    # save tensorflow checkpoint in subdir
+    def save_model(name=None): 
+        file_name = sub_dir + "/" + name
+        model_saver.save(sess=sess, save_path=utils.file_to_path(file_name), write_meta_graph=False, write_state=False)
+
+    # load tensorflow checkpoint from subdir
+    # returns true if checkpoint was found otherwise false
     def load_model(name=None):
         file_name = sub_dir + "/" + name
         try:
-            load_data = joblib.load(utils.file_to_path(file_name))
-        except IOError:
-            load_data = None
-
-        if load_data is not None:
-            params_dict = load_data['params']
-        else:
+            model_saver.restore(sess=sess, save_path=utils.file_to_path(file_name))
+        except ValueError:
             return False
-
-        if 'model' in params_dict:
-            loaded_params = params_dict['model']
-
-            loaded_params, params = utils.get_savable_params(loaded_params, 'model', keep_heads=True)
-
-            utils.restore_params(sess, loaded_params, params)
-            return True
-        else:
-            return False
+        return True
 
     def run(timesteps):
         obs = env.reset()
@@ -117,17 +115,14 @@ def main():
         return sum(rew_accum)
 
     def clean_exit():
-        utils.mpi_print("exit...")
+        utils.mpi_print("")
+        utils.mpi_print("== total duration", "{:.1f}".format(time.time() - t_first_start), " s ==")
+        utils.mpi_print(" exit...")
 
         # save best performing agent
         population.sort(key=lambda k: k['fit'], reverse=True) 
         load_model(name=population[0]["name"])
-        
-        # final evaluation
-        #for _ in range(10):
-        #    utils.mpi_print(run(timesteps_per_agent))
-
-        save_model()
+        dump_model()
 
         # cleanup
         sess.close()
@@ -141,13 +136,12 @@ def main():
         loaded_name = str(uuid.uuid1())
         save_model(name=loaded_name)
         
-
     # initialise population
     # either all random and no mutations pending
-    # or all from the restore point with all but one to be mutated
+    # or all from restore point with all but one to be mutated
     population = [{"name": loaded_name or str(uuid.uuid1()), "fit": -1, "need_mut": loaded_name != None and i != 0} for i in range(population_count)]
 
-    utils.mpi_print("== population size ", population_count, ", nenvs ", nenvs, ", t_agent ", timesteps_per_agent, " ==")
+    utils.mpi_print("== population size", population_count, ", t_agent ", timesteps_per_agent, " ==")
 
     t_first_start = time.time()
     try:
@@ -157,27 +151,29 @@ def main():
         while timesteps_done < total_timesteps:
             t_generation_start = time.time()
 
-            utils.mpi_print("__ gen ", generation, ", t_done ", timesteps_done, " __")
+            utils.mpi_print("")
+            utils.mpi_print("__ Generation", generation, " __")
 
             # initialise and evaluate all new agents
             for agent in population:
                 #if agent["fit"] < 0: # test/
                 if True: # test constant reevaluation, to dismiss "lucky runs" -> seems good
-                    
+
                     need_save = False
+                    # init new individuals
                     if not load_model(name=agent["name"]):
                         sess.run(model_init_op)
                         need_save = True
-
+                    # mutate if necessary
                     if agent["need_mut"]:
                         sess.run(model_noise_ops)
                         agent["need_mut"] = False
                         agent["name"] = str(uuid.uuid1())
                         need_save = True
-
+                    # save if necessary
                     if need_save:
                         save_model(name=agent["name"])
-                        
+                    # run agent
                     agent["fit"] = run(timesteps_per_agent)
 
                     timesteps_done += timesteps_per_agent
@@ -186,21 +182,24 @@ def main():
             # sort by fitness
             population.sort(key=lambda k: k['fit'], reverse=True) 
 
+            # print stuff
             utils.mpi_print(*["{:5}".format(int(agent["fit"])) for agent in population])
+            utils.mpi_print("__ average fit", "{:.1f}".format(np.mean([agent["fit"] for agent in population])),
+                            ", t_done", timesteps_done,
+                            ", took", "{:.1f}".format(time.time() - t_generation_start), "s",
+                            ", total", "{:.1f}".format(time.time() - t_generation_start), "s __")
 
-            utils.mpi_print("__ average", "{:.1f}".format(np.mean([agent["fit"] for agent in population])), " __")
-            utils.mpi_print("__ took", "{:.1f}".format(time.time() - t_generation_start), " s __")
-            utils.mpi_print("")
-
-            # cleanup to prevent hdd clutter
-            to_be_removed = set(os.listdir(utils.file_to_path(sub_dir))) - set([agent["name"] for agent in population])
+            # cleanup to prevent disk clutter
+            to_be_removed = set(re.sub('\..*$', '', f) for f in os.listdir(utils.file_to_path(sub_dir))) - set([agent["name"] for agent in population])
             for filename in to_be_removed: 
-                os.remove(utils.file_to_path(sub_dir+ "/" + filename))
+                os.remove(utils.file_to_path(sub_dir+ "/" + filename + ".index"))
+                os.remove(utils.file_to_path(sub_dir+ "/" + filename + ".data-00000-of-00001"))
 
+            # break when times up
             if not timesteps_done < total_timesteps:
                 break
         
-            # replace weak agents
+            # mark weak agents for replacement
             duplicate_factor =  1.0 / 16
             survive_factor = 1.0 / 4
             cutoff_duplicate = math.floor(population_count*duplicate_factor)
@@ -218,8 +217,6 @@ def main():
 
             generation += 1
         
-        utils.mpi_print(time.time() - t_first_start)
-
         clean_exit()
     except KeyboardInterrupt:
         clean_exit()
